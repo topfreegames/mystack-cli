498c4eae4075bdfe789e2283792dc9874b2975d7
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2017-04-25 11:21:22 -0300

    Support to custom domains

--- cmd/dns.go
+++ cmd/dns.go
@@ -9,6 +9,8 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"strings"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -22,6 +24,32 @@ type domainList struct {
 	Domains []string `json:"domains"`
 }
 
+func parseBody(body []byte, status int) (*domainList, error) {
+	if status != http.StatusOK {
+		errorBody := make(map[string]string)
+		err := json.Unmarshal(body, &errorBody)
+		if err != nil {
+			return nil, fmt.Errorf(string(body))
+		}
+
+		return nil, fmt.Errorf("Error: %s\nDescription: %s", errorBody["error"], errorBody["description"])
+	}
+
+	bodyMap := make(map[string][]string)
+	err := json.Unmarshal(body, &bodyMap)
+	if err != nil {
+		return nil, err
+	}
+
+	domainList := &domainList{}
+
+	for _, list := range bodyMap {
+		domainList.Domains = append(domainList.Domains, list...)
+	}
+
+	return domainList, nil
+}
+
 // dnsCmd represents the dns command
 var dnsCmd = &cobra.Command{
 	Use:   "dns",
@@ -29,6 +57,10 @@ var dnsCmd = &cobra.Command{
 	Long:  `starts a local dns server that will point all stack custom domains to mystack-router, e.g. test production apps`,
 	Run: func(cmd *cobra.Command, args []string) {
 		log := createLog()
+
+		if clusterName == "" {
+			log.Fatal("inform cluster name with -c flag")
+		}
 
 		c, err := models.ReadConfig(environment)
 		if err == nil {
@@ -42,16 +74,20 @@ var dnsCmd = &cobra.Command{
 		})
 		l.Debug("starting dns server, querying mystack controller for domains and pointTo")
 		l.Debug("grabbing mystack custom domain list and router ip from controller")
-		getDNSConfigURL := fmt.Sprintf("%s/dns", config.ControllerURL)
+		getDNSConfigURL := fmt.Sprintf("%s/cluster-configs/%s/domains", config.ControllerURL, clusterName)
 		client := models.NewMyStackHTTPClient(config)
-		body, _, err := client.Get(getDNSConfigURL)
+		body, status, err := client.Get(getDNSConfigURL)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		domainList := &domainList{}
-		json.Unmarshal(body, domainList)
+
+		domainList, err := parseBody(body, status)
+		if err != nil {
+			log.Fatal(err)
+		}
 		log.WithField("domains-list", domainList).Debug("got domain list from mystack")
-		server, err := models.NewDNSServer(domainList.Domains, forwardToDNS, config.ControllerURL, dnsPort, log)
+		controllerDomain := strings.TrimPrefix(config.ControllerHost, "controller.")
+		server, err := models.NewDNSServer(domainList.Domains, forwardToDNS, config.ControllerURL, controllerDomain, dnsPort, log)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -62,5 +98,6 @@ var dnsCmd = &cobra.Command{
 func init() {
 	dnsCmd.Flags().StringVarP(&forwardToDNS, "forwartToDNS", "f", "8.8.8.8:53", "The DNS to forward requests to when not in mystack custom domain list")
 	dnsCmd.Flags().IntVarP(&dnsPort, "port", "p", 53, "The port on which UDP Server will listen")
+	dnsCmd.Flags().StringVarP(&clusterName, "clusterName", "c", "", "Name of the cluster to be created")
 	RootCmd.AddCommand(dnsCmd)
 }
